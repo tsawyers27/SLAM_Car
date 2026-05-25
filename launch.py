d5eb3f97f85a43253e4c26e1f3d409dbb07c6bb6
tsawyers27 <sawyerstrevor@yahoo.com>
2026-05-25 19:20:49 -0400

    Simple launch file to test lidar

---
+++ launch.py
@@ -0,0 +1,16 @@
+import argparse
+import subprocess
+
+def main():
+    # Define parser to add terminal arguments and modes
+    parser = argparse.ArgumentParser()
+
+    # Add arguments and values to parser
+    parser.add_argument('mode', type=str, required=True,
+                        choices = ['lidar_test', 'slam', 'motor_test'])
+
+    # Control flow logic for different modes
+    args = parser.parse_args()
+
+    if args.mode == 'lidar_test':
+        subprocess.run(["ros2 launch rplidar_ros rplidar_a2m12_launch.py"])
