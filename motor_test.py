9105130123f07deea8c3e32a27b1a39948f9e074
tsawyers27 <sawyerstrevor@yahoo.com>
2026-06-02 16:22:22 -0400

    Updated motor_test.py with simpler test

--- motor_test.py
+++ motor_test.py
@@ -1,43 +1,32 @@
+from dcmotor import DCMotor
 from machine import Pin, PWM
 from time import sleep
 
-# Motor Pins
-in3 = Pin(19, Pin.OUT) #IN3 connected to GPIO 19
-in4 = Pin(20, Pin.OUT) #IN4 connected to GPIO 20
-enb = PWM(Pin(18))     #ENB connected to GPIO 18
-enb.freq(1000)
+frequency = 1000
 
-#Motor functions
-def motor_forward(speed):
-    in3.high()
-    in4.low()
-    enb.duty_u16(speed)
-    
-def motor_backward(speed):
-    in3.low()
-    in4.high()
-    enb.duty_u16(speed)
-    
-def motor_stop():
-    in3.low()
-    in4.low()
-    enb.duty_u16(0)
-    
-# Main loop to test motor
+pin1 = Pin(3, Pin.OUT)
+pin2 = Pin(4, Pin.OUT)
+enable = PWM(Pin(2), frequency)
+
+dc_motor = DCMotor(pin1, pin2, enable)
+
+# Set min duty cycle (15000) and max duty cycle (65535) 
+#dc_motor = DCMotor(pin1, pin2, enable, 15000, 65535)
+
 try:
-    while True:
-        print("Moving forward")
-        motor_forward(32768)
-        sleep(2)
-        
-        print("Stopping")
-        motor_stop()
-        sleep(2)
-        
-        print("Moving backward")
-        motor_backward(32768)
-        sleep(2)
-
+    print('Forward with speed: 50%')
+    dc_motor.forward(50)
+    sleep(5)
+    dc_motor.stop()
+    sleep(5)
+    print('Backwards with speed: 100%')
+    dc_motor.backwards(100)
+    sleep(5)
+    print('Forward with speed: 5%')
+    dc_motor.forward(5)
+    sleep(5)
+    dc_motor.stop()
+    
 except KeyboardInterrupt:
-    motor_stop()
-    print("Program stopped")
+    print('Keyboard Interrupt')
+    dc_motor.stop()
